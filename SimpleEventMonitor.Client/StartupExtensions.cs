8b0feb184c1fd8d85e351ab60c796498cdeaac03
Jesper Niedermann <jesper@niedermann.dk>
2016-03-05 00:18:26 +0100

    Startup class added to Client

---
+++ SimpleEventMonitor.Client/StartupExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Owin;
+
+namespace SimpleEventMonitor.Client
+{
+    public static class StartupExtensions
+    {
+        public static IAppBuilder Configuration(this IAppBuilder app)
+        {
+            app.Map("/signalr", map =>
+            {
+                map.UseCors(CorsOptions.AllowAll);
+                var hubConfiguration = new HubConfiguration();
+                map.RunSignalR(hubConfiguration);
+            });
+            return app;
+        }
+
+        public static IAppBuilder UseRedisBackplane(this IAppBuilder app, string redisHost, int redisPort, string redisPassword = "")
+        {
+            GlobalHost.DependencyResolver.UseRedis(redisHost, redisPort, redisPassword, "SimpleEventMonitor.SignalR.Backplane");
+            return app;
+        }
+    }
+
+}
