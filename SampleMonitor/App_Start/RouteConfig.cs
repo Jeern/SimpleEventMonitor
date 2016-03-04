9e9ac7346a5b272678e71615a49e442c1075f659
Jesper Niedermann <jesper@niedermann.dk>
2016-03-05 00:44:37 +0100

    Controller showed

--- SampleMonitor/App_Start/RouteConfig.cs
+++ SampleMonitor/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SampleMonitor
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Monitor", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
