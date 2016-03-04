b824861d68f5e293ab29ed99ac4c7a50ce9942cf
Jesper Niedermann <jesper@niedermann.dk>
2016-03-04 23:03:46 +0100

    A Nuget package deleted

---
+++ CommonItems/GlobalAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Jesper Niedermann")]
+[assembly: AssemblyProduct("SimpleEventMonitor")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.5.0.0")]
