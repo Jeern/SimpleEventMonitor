6725ef90b12a60276de6cc80ef8fd8e1ac914c39
Jesper Niedermann <jesper@niedermann.dk>
2016-03-11 22:50:23 +0100

    Can get events

---
+++ SimpleEventMonitor.Sample/GetEventsRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ServiceStack.ServiceHost;
+using SimpleEventMonitor.Common;
+
+namespace SimpleEventMonitor.Sample
+{
+    [Route("/Events", "GET")]
+    public class GetEventsRequest : IReturn<IEnumerable<SimpleEvent>>
+    {
+    }
+}
---
+++ SimpleEventMonitor.Sample/GetEventsService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using ServiceStack.ServiceInterface;
+using SimpleEventMonitor.Common;
+using SimpleEventMonitor.Store.Redis;
+
+namespace SimpleEventMonitor.Sample
+{
+    public class GetEventsService : Service
+    {
+        public IEnumerable<SimpleEvent> Get(GetEventsRequest request)
+        {
+            var eventDataStore = new RedisEventDataStore();
+            return eventDataStore.GetEvents();
+        }
+    }
+}
