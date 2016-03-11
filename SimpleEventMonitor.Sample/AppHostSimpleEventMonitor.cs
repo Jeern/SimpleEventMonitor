bc5837c8d69da8bfca129ebadf7c5ce190aaf8d8
Jesper Niedermann <jesper@niedermann.dk>
2016-03-11 22:44:45 +0100

    Sample Service can start

---
+++ SimpleEventMonitor.Sample/AppHostSimpleEventMonitor.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using Funq;
+using ServiceStack.Common.Web;
+using ServiceStack.ServiceHost;
+using ServiceStack.Text;
+using ServiceStack.WebHost.Endpoints;
+
+namespace SimpleEventMonitor.Sample
+{
+    public class AppHostSimpleEventMonitor : AppHostBase
+    {
+        public AppHostSimpleEventMonitor()
+            : base("SimpleEventMonitorServices", Assembly.GetExecutingAssembly())
+        {
+        }
+
+        public override void Configure(Container container)
+        {
+            //JsConfig.AlwaysUseUtc = true;
+            //JsConfig.DateHandler = JsonDateHandler.ISO8601;
+            JsConfig.EmitCamelCaseNames = true;
+            JsConfig.IncludeNullValues = true;
+
+            SetConfig(
+                    new EndpointHostConfig
+                    {
+                        DebugMode = true,
+                        DefaultContentType = ContentType.Json,
+                        EnableFeatures = Feature.All
+                    });
+        }
+    }
+}
---
+++ SimpleEventMonitor.Sample/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Owin;
+using SimpleEventMonitor.Client;
+using SimpleEventMonitor.Store.Redis;
+
+namespace SimpleEventMonitor.Sample
+{
+    public class Startup
+    {
+        private AppHostSimpleEventMonitor _appHost;
+        public void Configuration(IAppBuilder app)
+        {
+            _appHost = new AppHostSimpleEventMonitor();
+            _appHost.Init();
+            app.ConfigureMonitor(() => new RedisEventDataStore());
+        }
+    }
+}
