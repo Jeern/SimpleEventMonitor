876e29c7c2d440cd0a038f89d22ae1b6dc6aad0f
Jesper Niedermann <jesper@niedermann.dk>
2016-03-12 00:10:20 +0100

    Events loading again

---
+++ SimpleEventMonitor.Client/SignalRHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using SimpleEventMonitor.Common;
+using SimpleEventMonitor.Store.Redis;
+
+namespace SimpleEventMonitor.Client
+{
+    [HubName("EventHub")]
+    public class SignalRHub : Hub
+    {
+        private readonly IEventDataStore _dataStore;
+        private string _groupId;
+
+        public SignalRHub()
+        {
+            _dataStore = new RedisEventDataStore();
+//            _dataStore.SimpleEventHappened += (sender, args) => Clients.All.publish(args.Evt);
+            _dataStore.SimpleEventHappened += (sender, args) => Clients.Group(_groupId).publish(args.Evt);
+        }
+
+        public void Subscribe(string groupId)
+        {
+            _groupId = groupId;
+            Groups.Add(Context.ConnectionId, groupId);
+        }
+    }
+}
