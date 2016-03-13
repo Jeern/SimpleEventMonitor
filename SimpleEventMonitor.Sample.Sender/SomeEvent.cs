7dba7dd35aa24a70c593e3fc6296a4b166000259
Jesper Niedermann <jesper@niedermann.dk>
2016-03-13 22:26:53 +0100

    SomeEvent moved

---
+++ SimpleEventMonitor.Sample.Sender/SomeEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleEventMonitor.Sample.Sender
+{
+    public class SomeEvent
+    {
+        public SomeEvent()
+        {
+            Id = Guid.NewGuid();
+            HappenedAt = DateTime.UtcNow;
+        }
+
+        public Guid Id { get; set; }
+        public DateTime HappenedAt { get; set; }
+    }
+}
