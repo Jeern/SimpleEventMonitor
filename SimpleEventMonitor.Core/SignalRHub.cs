afc15f4d05b307511f971e1e8c9967cd147ad604
Jesper Niedermann <jesper@niedermann.dk>
2016-03-13 16:50:42 +0100

    Stuff deleted

--- SimpleEventMonitor.Core/SignalRHub.cs
+++ SimpleEventMonitor.Core/SignalRHub.cs
@@ -9,28 +9,8 @@ namespace SimpleEventMonitor.Core
 
         public void Subscribe(string groupId)
         {
-            //Task.Factory.StartNew(() =>
-            //{
-            //    Thread.Sleep(1500);
-            //    Clients.All.publish(new SimpleEvent(new SomeEvent()));
-            //    Clients.Group(groupId).publish(new SimpleEvent(new SomeEvent()));
-            //    Thread.Sleep(2500);
-            //    Clients.All.publish(new SimpleEvent(new SomeEvent()));
-            //    Clients.Group(groupId).publish(new SimpleEvent(new SomeEvent()));
-            //    Thread.Sleep(3500);
-            //    Clients.All.publish(new SimpleEvent(new SomeEvent()));
-            //    Clients.Group(groupId).publish(new SimpleEvent(new SomeEvent()));
-            //    Thread.Sleep(4500);
-            //    Clients.All.publish(new SimpleEvent(new SomeEvent()));
-            //    Clients.Group(groupId).publish(new SimpleEvent(new SomeEvent()));
-            //    Thread.Sleep(5500);
-            //    Clients.All.publish(new SimpleEvent(new SomeEvent()));
-            //    Clients.Group(groupId).publish(new SimpleEvent(new SomeEvent()));
-            //});
-
             Groups.Add(Context.ConnectionId, groupId);
         }
-
 
         public void Publish(SimpleEvent evt)
         {
