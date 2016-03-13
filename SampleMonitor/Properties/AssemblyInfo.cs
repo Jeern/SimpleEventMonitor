adfaffdea450d45d8c9704c04f35be5d442296f9
Jesper Niedermann <jesper@niedermann.dk>
2016-03-13 16:12:29 +0100

    Deleted old files

--- SampleMonitor/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using Microsoft.Owin;
-
-[assembly: AssemblyTitle("SampleMonitor")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: Guid("ab485421-0b79-4115-9579-c819633a5b48")]
-[assembly: OwinStartup(typeof(SampleMonitor.Startup))]
--- SimpleEventMonitor.Server/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("SimpleEventMonitor.Server")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: Guid("c7ced39a-9a53-4dfe-a0b7-ba0556b62064")]
