c6975ffbe6361b2fe1c635a0f4174b72fed363de
Jeern <jesper@niedermann.dk>
2016-04-10 20:12:48 +0200

    New Object to namespace the functions

--- SimpleEventMonitor.Sample/Scripts/SimpleEventMonitor.js
+++ SimpleEventMonitor.Sample/Scripts/SimpleEventMonitor.js
@@ -1,10 +1,13 @@
-﻿function setupSignalR(dataAdder) {
-    var connection = $.hubConnection('/signalr');
-    var eventHubProxy = connection.createHubProxy('eventHub');
-    eventHubProxy.on('publish', function (data) {
-        dataAdder(data);
-    });
-    connection.start().done(function () {
-        eventHubProxy.invoke('subscribe', 'simpleEventMonitorEvents');
-    });
+﻿var SEM = {
+
+    setupSignalR : function (dataAdder) {
+        var connection = $.hubConnection('/signalr');
+        var eventHubProxy = connection.createHubProxy('eventHub');
+        eventHubProxy.on('publish', function (data) {
+            dataAdder(data);
+        });
+        connection.start().done(function () {
+            eventHubProxy.invoke('subscribe', 'simpleEventMonitorEvents');
+        });
+    }
 }
