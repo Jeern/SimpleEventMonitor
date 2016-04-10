9880e134b351f95be1a7abc6eeac13ac51b70145
Jeern <jesper@niedermann.dk>
2016-04-10 19:59:51 +0200

    Moved SetupSignalR function to common file

---
+++ SimpleEventMonitor.Sample/Scripts/SimpleEventMonitor.js
@@ -0,0 +1,10 @@
+﻿function setupSignalR(dataAdder) {
+    var connection = $.hubConnection('/signalr');
+    var eventHubProxy = connection.createHubProxy('eventHub');
+    eventHubProxy.on('publish', function (data) {
+        dataAdder(data);
+    });
+    connection.start().done(function () {
+        eventHubProxy.invoke('subscribe', 'simpleEventMonitorEvents');
+    });
+}
